6ad80c73fd78758b1c060fb1ffeb5eb995c7c66b
Okonos <tepyzboj@gmail.com>
2017-02-25 15:05:25 +0100

    Add aliases file

---
+++ oh-my-zsh/.oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,11 @@
+alias vide="byobu-layout restore IDE-vertical"
+alias diff="colordiff"
+
+# formatting
+alias mount="mount | column -t"
+
+# prompt for confirmation when deleting more than 3 files at a time
+alias rm="rm -I --preserve-root"
+
+# xclip - always select clipboard
+alias xclip="xclip -selection c"
