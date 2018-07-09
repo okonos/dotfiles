160183db39d51d74f75a3ce478fb86171851294c
Okonos <tepyzboj@gmail.com>
2018-07-09 22:15:53 +0200

    aliases -- remove mount alias

--- oh-my-zsh/.oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/.oh-my-zsh/custom/aliases.zsh
@@ -1,9 +1,6 @@
 alias vide="byobu-layout restore IDE-vertical"
 alias diff="colordiff"
 
-# formatting
-alias mount="mount | column -t"
-
 # prompt for confirmation when deleting more than 3 files at a time
 alias rm="rm -I --preserve-root"
 
