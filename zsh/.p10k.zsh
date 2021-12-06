0261b576c25e24dd5cd81cbc5bae7d5f766fce4d
Okonos <tepyzboj@gmail.com>
2021-12-06 12:11:27 +0100

    [p10k] Move background jobs prompt element to left side

--- zsh/.p10k.zsh
+++ zsh/.p10k.zsh
@@ -32,6 +32,7 @@
   # The list of segments shown on the left. Fill it with the most important segments.
   typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
     # os_icon               # os identifier
+    background_jobs         # presence of background jobs
     virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
     dir                     # current directory
     vcs                     # git status
@@ -45,7 +46,6 @@
   typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
     status                  # exit code of the last command
     command_execution_time  # duration of the last command
-    background_jobs         # presence of background jobs
     direnv                  # direnv status (https://direnv.net/)
     asdf                    # asdf version manager (https://github.com/asdf-vm/asdf)
     # anaconda                # conda environment (https://conda.io/)
